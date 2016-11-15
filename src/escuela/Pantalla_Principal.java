a8295748450acde760e191969644290877ac5d4c
Pamasoli19 <jpmscr19@gmail.com>
2016-11-15 12:12:56 -0600

    Aporte Pablo,..3

--- src/escuela/Pantalla_Principal.java
+++ src/escuela/Pantalla_Principal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package escuela;
+package escuela;//Parte grafica Estudiante
 
 import Estudiante.Estudiante;
 import java.sql.SQLException;
