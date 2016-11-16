2bf5180cf1112205d33b3c0973a5ffe10b04806e
Pamasoli19 <jpmscr19@gmail.com>
2016-11-16 12:03:57 -0600

    Aporte Pablo,..2

--- src/ConexionBD/conexión_postgres.java
+++ src/ConexionBD/conexión_postgres.java
@@ -6,7 +6,7 @@ package ConexionBD;
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.SQLException;//Aporte grafico
 
 /**
  *
